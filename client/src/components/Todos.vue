1485d65d781bea787bb4a1d9718ec36a8bc7a98e
im83 <programmer38rus@gmail.com>
2020-06-18 23:31:26 +0800

    Допильть tbody

--- client/src/components/Todos.vue
+++ client/src/components/Todos.vue
@@ -5,6 +5,17 @@
       <button type="button" id="task-add" class="btn btn-success btn-sm align-left d-block">
         Добавить задачу
       </button>
+      <table class="table table-dark table-table-stripped table-hover">
+        <thead class="thead-light">
+          <tr>
+            <th>Uid</th>
+            <th>Описание</th>
+            <th>Выполнена?</th>
+            <th></th>
+          </tr>
+        </thead>
+
+      </table>
     </div>
   </div>
 </template>
