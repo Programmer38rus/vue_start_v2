b3fd7c0c803578ec250f0a4e812e07419c381338
Vyacheslav <Programmer38rus@gmail.com>
2020-06-19 16:25:06 +0800

    Подвязваем функционал для интерфейса

--- client/src/components/Todos.vue
+++ client/src/components/Todos.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
     <div class="col-sm-10">
-      <h1>Задачи</h1>
+      <h1>TASKS</h1>
       <button type="button" id="task-add" class="btn btn-success btn-sm align-left d-block">
         Добавить задачу
       </button>
+
       <table class="table table-dark table-table-stripped table-hover">
+
         <thead class="thead-light">
           <tr>
             <th>Uid</th>
@@ -15,13 +17,107 @@
           </tr>
         </thead>
 
+        <tbody>
+          <tr v-for="(todo, index) in todos" :key="index">
+            <td class="todo-uid">{{ todo.uid }}</td>
+            <td>{{ todo.description }} </td>
+            <td>
+              <span v-if="todo.is_complited">Выполнено</span>
+              <span v-else>Не выполнено</span>
+            </td>
+            <td>
+              <div class="btn-group" role="group">
+                <button type="button" class="btn btn-secondary btn-sm">Обновить</button>
+                &nbsp;
+                <button type="button" class="btn btn-danger btn-sm">X</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+
       </table>
+
     </div>
+    <b-modal ref="addTodoModal"
+      id="todo-modal"
+      title="Добавить задачу"
+      hide-footer>
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+        <b-form-group id="form-description-group"
+          label="Описание"
+          label-for="form-descrition-input">
+          <b-form-input id="form-description-input"
+            type="text"
+            v-model="addTodoForm.description"
+            required
+            placeholder="Завести задачу">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="form-complete-group">
+          <b-form-checkbox-group v-model="addTodoForm.is_completed" id="form-checks">
+            <b-form-checkbox value="true">Задача выполнена?</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Добавить</b-button>
+        <b-button type="reset" variant="danger">Сброс</b-button>
+      </b-form>
+    </b-modal>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+const todoListURL = 'http://localhost:5000/api/tasks';
+const todoAddURL = 'http://localhost:5000/api/add-task/';
+
 export default {
-  name: 'Todos',
+  name: 'Todo',
+  data() {
+    return {
+      todos: [],
+      addTodoForm: {
+        description: '',
+        is_completed: [],
+      }
+    };
+  },
+
+  methods: {
+    getMessage() {
+      axios.get(todoListURL)
+        .then((response) => {
+          this.todos = response.data.tasks;
+          console.log.table(this.todos);
+        });
+    },
+    resetForm() {
+      this.addTodoForm.description = '';
+      this.addTodoForm.is_completed = [];
+    },
+    onSubmit(event) {
+      event.preventDefault();
+      this.$refs.addTodoModal.hide();
+      const requestData = {
+        description: this.addTodoForm.description,
+      }
+    },
+  },
+  created() {
+    this.getMessage();
+  },
 };
 </script>
+
+<style>
+button#task-add{
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+h2, td {
+  text-align: left;
+}
+  .todo-uid {
+    text-align: right;
+  }
+</style>
