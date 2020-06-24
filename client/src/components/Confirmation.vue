dc8e09881d3311b5816bbe1aebb4a11b61ce871e
Vyacheslav <Programmer38rus@gmail.com>
2020-06-24 14:28:07 +0800

    Код на перевале

--- client/src/components/Confirmation.vue
+++ client/src/components/Confirmation.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="alert alert-success alert-dismissible fade show" role="alert">
+  <div id="hider" class="alert alert-success alert-dismissible fade show" role="alert">
     Бутстап подключен и работает
     <button type="button" class="close" data-dismiss="alert" aria-label="Close"
-            @click="$on.hide('#alert')">
+           v-b-toggle:hider>
       <span aria-hidden="true">&times;</span>
     </button>
   </div>
