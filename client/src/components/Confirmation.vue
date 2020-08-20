a2dbb0db9ff452a0fc9a4ab59b1bf766baf51f8b
Vyacheslav <Programmer38rus@gmail.com>
2020-08-20 11:34:18 +0800

    set up work Todos on db sqlalchemy

--- client/src/components/Confirmation.vue
+++ client/src/components/Confirmation.vue
@@ -1,32 +1,21 @@
 <template>
   <div>
     <div>
-      <b-alert ref="hider" variant="success"
-        show
-        dismissible
-        fade
-      >{{ message }}
-          <b>
-          </b>
-      </b-alert>
+        <b-alert ref="hider" variant="success"
+          show
+          dismissible
+          fade
+        >{{ message }}
+            <b>
+            </b>
+        </b-alert>
     </div>
   </div>
 </template>
 
 <script>
-// import Todos from './Todos.vue';
 export default {
   name: 'Confirmation',
   props: ['message'],
-  data() {
-    return {
-      dismissSecs: 5,
-      dismissCountDown: 0,
-      showDismissibleAlert: false,
-    };
-  },
-  methods: {
-
-  },
 };
 </script>
--- client/src/views/server.py
+++ client/src/views/server.py
@@ -4,43 +4,9 @@ from db_operations import form_list, add_task_to_db, delete_task, change_task
 
 app = bottle.Bottle()
 
-# class Todoitem:
-#     def __init__(self, descriprion, unique_id):
-#         self.description = descriprion
-#         self.is_completed = False
-#         self.uid = unique_id
-#
-#     def __str__(self):
-#         return self.description.lower()
-#
-#     def to_dict(self):
-#         return {
-#             "description": self.description,
-#             "is_completed": self.is_completed,
-#             "uid": self.uid
-#         }
-#
-#
-# task_list = ["написать код",
-#              "покормить морскую свинку",
-#              "постирать кроссовки",
-#              "поесть еды"
-#              ]
-#
-# tasks_db = {
-#     uid: Todoitem(desc, uid)
-#     for uid, desc in enumerate(iterable=task_list, start=1)
-# }
-# print(tasks_db)
-
 @enable_cors
 @app.route("/api/tasks/")
 def index():
-    # tasks = [task.to_dict() for task in tasks_db.values()]
-    # for i in tasks_db.values():
-    #     print(i)
-    # return {"tasks": tasks}
-
     db_list = form_list()
     db_on_dict = [child.to_dict() for child in db_list]
 
@@ -49,15 +15,6 @@ def index():
 @enable_cors
 @app.route("/api/add-task/", method="POST")
 def add_task():
-    # desc = bottle.request.json['description']
-    # is_completed = bottle.request.json['is_completed']
-    #
-    # if len(desc) > 0:
-    #     new_uid = max(tasks_db.keys()) + 1
-    #     t = Todoitem(desc, new_uid)
-    #     t.is_completed = is_completed
-    #     tasks_db[new_uid] = t
-    # return "OK"
     json = bottle.request.json
     add_task_to_db(json)
 
@@ -72,28 +29,15 @@ def show_or_modify_task(uid):
         print("we is in the func!")
         return tasks_db[uid].to_dict()
     elif bottle.request.method == "PUT":
-        # if "description" in bottle.request.json:
-        #     tasks_db[uid].description = bottle.request.json["description"]
-        # if "is_completed" in bottle.request.json:
-        #     tasks_db[uid].is_completed = bottle.request.json["is_completed"]
         task = bottle.request.json
         print(task)
         change_task(uid, task)
 
         return f'Modified task {uid}'
     elif bottle.request.method == "DELETE":
-        # tasks_db.pop(uid)
         delete_task(uid)
 
         return f"Deleted task {uid}"
-
-# @bottle.route("/api/delete/<uid:int>")
-# def api_delete(uid):
-#     tasks_db.pop(uid)
-#     return "OK"
-#     delete_task(uid)
-
-
 
 @bottle.route("/api/complete/<uid:int>")
 def api_complete(uid):
@@ -102,6 +46,5 @@ def api_complete(uid):
 
 app.install(CorsPlugin(origins=['http://localhost:5000']))
 
-###
 if __name__ == "__main__":
     bottle.run(app, host="localhost", port=5000)
